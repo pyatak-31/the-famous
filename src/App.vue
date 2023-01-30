3f3934f6de2ff7581813eb53b453e49b800116cc
Oleg <pyatak.ov@yandex.ru>
2023-01-30 11:43:30 +0300

    add global style settings

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<img alt="Vue logo" src="./assets/logo.png">
+	<HelloWorld msg="Welcome to Your Vue.js App"/>
+
+	<p class="text">HAO</p>
 </template>
 
 <script>
@@ -15,12 +17,9 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	.text {
+		font-family: Merriweather;
+		font-weight: 700;
+		// font-weight: 300;
+	}
 </style>
